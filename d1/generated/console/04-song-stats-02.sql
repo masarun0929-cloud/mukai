429ce27d60c197adf0c45610f6ef8a61b7ec9fb8
masarun0929-cloud <masarun0929@gmail.com>
2026-05-30 13:35:04 +0900

    Update 04-song-stats-02.sql

--- d1/generated/console/04-song-stats-02.sql
+++ d1/generated/console/04-song-stats-02.sql
@@ -1,6 +1,3 @@
--- Console chunk: song-stats 2
--- Run d1/schema.sql first. Then run console chunks in file-name order.
-
 INSERT INTO song_channel_stats (song_id, channel_id, sing_count, source_index, updated_at) VALUES ((SELECT id FROM songs WHERE song_key = '君とtea for two♡__わーすた'), (SELECT id FROM channels WHERE code = 'new'), 1, 62, CURRENT_TIMESTAMP)
 ON CONFLICT(song_id, channel_id) DO UPDATE SET
   sing_count = excluded.sing_count,
